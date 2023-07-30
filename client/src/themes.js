8e1b94037d05f3d647a56aeb987e865df1f823a9
bitmapstr <bitmapstr@gmail.com>
2023-07-29 22:10:11 -0400

    Create themes.js

---
+++ client/src/themes.js
@@ -0,0 +1,10 @@
+export const themes = [
+    { name: 'LIGHT PINK', value: 25 },
+    { name: 'PINK-WHITE-RED', value: 75 },
+    { name: 'PINK-RED', value: 100 },
+    { name: 'LIGHT PINK', value: 150 },
+    { name: 'ORANGY AUTUMN', value: 256 },
+    { name: 'AQUA BLUES', value: 500 }
+    // Add more themes or modify existing ones as needed
+  ];
+  

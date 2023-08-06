45310af4e62c754d19bca4f9fc862f9cc1450d4a
bitmapstr <bitmapstr@gmail.com>
2023-08-06 14:15:22 -0400

    Update SidebarMenuItem2.svelte

--- client/src/components/SidebarMenuItem2.svelte
+++ client/src/components/SidebarMenuItem2.svelte
@@ -1,10 +1,9 @@
 <script>
-import { tick } from 'svelte'
 import Toggle from './util/Toggle.svelte'
 import Pill from './util/Pill.svelte'
 import Select from 'svelte-select'
 import { createEventDispatcher } from 'svelte'
-import { freezeResize, currentColor1 } from '../stores.js'
+import { freezeResize } from '../stores.js'
 const dispatch = createEventDispatcher()
 
 export let value = false
@@ -44,7 +43,7 @@ async function focusOut(e) {
 </script>
 
 <style type="text/scss">
-  .sidebar-menu-item {
+  .sidebar-menu-item2 {
     display: flex;
     flex-direction: row;
     justify-content: space-between;
@@ -82,7 +81,7 @@ async function focusOut(e) {
   }
 </style>
 
-<div class="sidebar-menu-item" class:active={value}  on:click>
+<div class="sidebar-menu-item2" class:active={value}  on:click>
   {#if type === 'pill'}
     <span class="label">{ label }</span>
     <Pill active={value} left={falseLabel} right={trueLabel} />

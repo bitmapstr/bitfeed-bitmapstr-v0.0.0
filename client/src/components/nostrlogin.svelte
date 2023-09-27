03acb6fea40076d90de372bb009cd3f873279d8d
bitmapstr <137800231+bitmapstr@users.noreply.github.com>
2023-09-27 18:39:02 -0400

    Revert "nostr stuff"
    This reverts commit 34f35c4990873c1b1e825a4e75850ee11d9e7ec8.

--- client/src/components/nostrlogin.svelte
+++ client/src/components/nostrlogin.svelte
@@ -1,38 +1,29 @@
 <script>
 import  {currentColor1 } from "../stores";
+
+    // Import the package
 import NDK, { NDKNip07Signer, NDKEvent } from "@nostr-dev-kit/ndk";
 
 
-var relays = ["ws://umbrel.local:4848"]
-const socket = new WebSocket(relays[0])
 
-const nip07signer = new NDKNip07Signer();
-const ndk = new NDK({ signer: nip07signer, explicitRelayUrls: relays, });
 
 async function nostrConnect() {
-  console.log("Relay: ")
-  console.log(socket)
-  ndk.connect()
+    const nip07signer = new NDKNip07Signer();
+    const ndk = new NDK({ signer: nip07signer, explicitRelayUrls: ["ws://umbrel.local:4848", " wss://relay.snort.social/", "wss://nos.lol/"], });
     nip07signer.user().then(async (user) => {
     if (!!user.npub) {
         console.log("Permission granted to read their public key:", user.npub);
     }
 });
-}
 
-
-function publishEvent() {
-  const ndkEvent = new NDKEvent(ndk);
+    const ndkEvent = new NDKEvent(ndk);
     ndkEvent.kind = 1;
     ndkEvent.content = "rad everything!";
-    // ndkEvent.publish(); // This will trigger the extension to ask the user to confirm signing.
+    ndkEvent.publish(); // This will trigger the extension to ask the user to confirm signing.
     console.log("ndkEvent")
     console.log(ndkEvent)
 
-  
 }
-
-
 
 
 </script>
@@ -58,6 +49,5 @@ function publishEvent() {
 <div>
 <h1>Nostr login</h1>
 <button class="primary" style="background-color: {$currentColor1}" on:click={nostrConnect}>log-in</button>
-<button class="primary" style="background-color: {$currentColor1}" on:click={publishEvent}>publish</button>
 
 </div>
